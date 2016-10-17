c08950f1fb2db6a5fc1cf72ebf14aa5f8ca4382f
goooon <goooon@126.com>
2016-10-17 18:22:18 +0800

    add dep.cpp

---
+++ tbox/src/cpp/dep.cpp
@@ -0,0 +1,99 @@
+#include "../inc/dep.h"
+#include "../inc/Application.h"
+#ifdef ME_STDIO
+using namespace me;
+lua_State* g_state;
+me::CmdConsole con;
+Application* getApp() {
+	return &Application::getInstance();
+}
+void conned() {
+	PostEvent(AppEvent::NetConnected, 0, 0, 0);
+}
+void disced() {
+	PostEvent(AppEvent::NetDisconnected, 0, 0, 0);
+}
+void* initDebugLib()
+{
+	using namespace luabridge;
+	using namespace me;
+	me::Memory::setCheckMemory(vTrue);
+	me::Singleton::initAll();
+	me::Trace::addTracer(&con);
+	g_state = luaL_newstate();         //创建lua运行环境
+	if (g_state){
+		const lua_Number* n = lua_version(g_state);
+		LOG_I("lua %d create ...", *n);
+		luaL_openlibs(g_state);
+		
+		getGlobalNamespace(g_state).beginNamespace("me")
+			.addFunction("getApp", &getApp)
+			.addFunction("netOk", &conned)
+			.addFunction("netFail", &disced)
+			.beginClass<Application>("Application")
+			.addFunction("connect", &Application::connectServer)
+			.addFunction("disconnect", &Application::disconnectServer)
+			.endClass();
+	}
+	return &con;
+}
+void uninitDebugLib(void* lib)
+{
+	if (g_state) {
+		lua_close(g_state);
+	}
+	Singleton::destroyAll();
+	me::Trace::finalise();
+	Memory::dispalyInfo();
+	Memory::checkMemory(vTrue);
+}
+#include "../../console/dxmain.cpp"
+
+void onCommand(char* cmd)
+{
+	if (me::Tool::isEqual(cmd, "v")) {
+		me::Trace::setLevelMask(me::Trace::getLevelMask() & me::Trace::Level::V ? me::Trace::Level::MI : me::Trace::Level::MV);
+		return;
+	}
+	if (me::Tool::isEqual(cmd, "i")) {
+		me::Trace::setLevelMask(me::Trace::getLevelMask() & me::Trace::Level::I ? me::Trace::Level::MW : me::Trace::Level::MI);
+		return;
+	}
+	if (me::Tool::isEqual(cmd, "m")) {
+		me::Trace::setLevelMask(me::Trace::getLevelMask() & me::Trace::Level::M ? me::Trace::Level::MM : me::Trace::Level::MM);
+		return;
+	}
+	if (me::Tool::isEqual(cmd, "e")) {
+		me::Trace::setLevelMask(me::Trace::getLevelMask() & me::Trace::Level::E ? me::Trace::Level::ME : me::Trace::Level::ME);
+		return;
+	}
+	if (me::Tool::isEqual(cmd, "clear")) {
+		con.clear();
+		return;
+	}
+	if (Application::getInstance().onDebugCommand(cmd))return;
+	if (g_state) {
+		int r = luaL_dostring(g_state, cmd);
+		if (r)
+		{
+			LOG_E("Lua return false %s", lua_tostring(g_state, -1));
+			lua_pop(g_state, 1); // remove error message
+			return;
+		}
+	}
+}
+#else
+void* initDebugLib() { return 0; }
+void uninitDebugLib(void* lib){}
+
+LoopCallback debugMain(int argc, char* argv[])
+{
+	return 0;
+}
+
+void onCommand(char* cmd)
+{
+	//g_papp->onCommand(cmd);
+}
+
+#endif
