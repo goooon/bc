07e45785e4a9e87615915b280961b69c29449b31
lizhoujian <lizhoujian123@163.com>
2016-10-19 15:19:04 +0800

    add cmake dir for cross platform building

---
+++ cmake/toolchain.linux-arm11.cmake
@@ -0,0 +1,11 @@
+# path to compiler and utilities
+# specify the cross compiler
+SET(CMAKE_C_COMPILER arm-none-linux-gnueabi-gcc)
+SET(CMAKE_CXX_COMPILER arm-none-linux-gnueabi-g++)
+
+# Name of the target platform
+SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Version of the system
+SET(CMAKE_SYSTEM_VERSION 1)
